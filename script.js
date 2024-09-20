613c9a5146cc838f87402e635b2ce80d7712d146
victoriaa2012 <marlyvictoriaa@gmail.com>
2024-09-20 08:42:08 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+function incorrecto(){
+
+alert("respuesta inccorrecta");}
+
+function mostrarpre2(){
+
+    let ocultar=document.getElementById("pre1")
+    let mostrar=document.getElementById("pre2")
+
+    ocultar.style.display="none"
+    mostrar.style.display="flex"
+}
+
+function mostrarpre3(){
+
+    let ocultar=document.getElementById("pre2")
+    let mostrar=document.getElementById("pre3")
+
+    ocultar.style.display="none"
+    mostrar.style.display="flex"
+}
+
+function mostrarpre4(){
+
+    let ocultar=document.getElementById("pre3")
+    let mostrar=document.getElementById("pre4")
+
+    ocultar.style.display="none"
+    mostrar.style.display="flex"
+}
+
+function mostrarpre5(){
+
+    let ocultar=document.getElementById("pre4")
+    let mostrar=document.getElementById("pre5")
+
+    ocultar.style.display="none"
+    mostrar.style.display="flex"
+}
+
+function mostrarcorrecto(){
+
+    let ocultar=document.getElementById("pre5")
+    let mostrar=document.getElementById("bien")
+
+    ocultar.style.display="none"
+    mostrar.style.display="flex"
+}
